492cc5db62392647b3384af299d4377128a37247
Tony Smith (smittytone) <tony@smittytone.net>
2025-09-09 15:59:31 +0100

    Migrate extensions to separate file

--- Sources/Highlighter/Extensions.swift
+++ Sources/Highlighter/Extensions.swift
@@ -5,4 +5,76 @@
 //  Created by Tony Smith on 09/09/2025.
 //
 
-import Foundation
+import AppKit
+
+
+extension NSMutableAttributedString {
+
+    /**
+     Swap the paragraph style in all of the attributes of
+     an NSMutableAttributedString.
+
+     - Parameters:
+        - paraStyle: The injected NSParagraphStyle.
+    */
+    func addParaStyle(with paraStyle: NSParagraphStyle) {
+        beginEditing()
+        self.enumerateAttribute(.paragraphStyle, in: NSMakeRange(0, self.length)) { (value, range, stop) in
+            if let _ = value as? NSParagraphStyle {
+                removeAttribute(.paragraphStyle, range: range)
+                addAttribute(.paragraphStyle, value: paraStyle, range: range)
+            }
+        }
+        endEditing()
+    }
+}
+
+
+extension NSAttributedString {
+
+    /**
+     Split the instance as per splitting a regular string.
+     */
+    func components(separatedBy separator: String) -> [NSAttributedString] {
+        var parts: [NSAttributedString] = []
+        let subStrings = self.string.components(separatedBy: separator)
+        var range = NSRange(location: 0, length: 0)
+        for string in subStrings {
+            range.length = string.utf16.count
+            let attributedString = attributedSubstring(from: range)
+            parts.append(attributedString)
+            range.location += range.length + separator.utf16.count
+        }
+        return parts
+    }
+}
+
+
+extension Scanner {
+
+    /**
+     Look ahead and return the next character in the sequence without
+     altering the current location of the scanner.
+
+     - Parameters
+        - in: The string being scanned.
+
+     - Returns The next character as a string.
+     */
+    func getNextCharacter(in outer: String) -> String {
+
+        let string: NSString = self.string as NSString
+        let idx: Int = self.currentIndex.utf16Offset(in: outer)
+        let nextChar: String = string.substring(with: NSMakeRange(idx, 1))
+        return nextChar
+    }
+
+
+    /**
+     Step over the next character.
+     */
+    func skipNextCharacter() {
+
+        self.currentIndex = self.string.index(after: self.currentIndex)
+    }
+}
